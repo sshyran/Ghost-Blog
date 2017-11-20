a0bd6ec4c90cca00deb479d3da458af87941a9e5
Aileen Nowak <AileenCGN@gmail.com>
2017-11-20 13:29:21 +0800

    Revert "🛠  Gulp deploy"
    This reverts commit 94e10122bc58e85aa4bb4be2215cc3d4409c12aa.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var nodemon = require('gulp-nodemon');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
-var gitignore = require('gulp-gitignore');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -15,16 +14,6 @@ var colorFunction = require('postcss-color-function');
 var cssnano = require('cssnano');
 var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
-
-// dependencies for deployment
-var fs = require('fs');
-var path = require('path');
-var got = require('got');
-var FormData = require('form-data');
-
-// Shared variables
-var themeZipPath = '.dist';
-var themeZipName = require('./package.json').name + '.zip';
 
 var swallowError = function swallowError(error) {
     gutil.log(error.toString());
@@ -48,7 +37,8 @@ gulp.task('css', function () {
         autoprefixer({browsers: ['last 2 versions']}),
         cssnano()
     ];
-    gulp.src('assets/css/*.css')
+
+    return gulp.src('assets/css/*.css')
         .on('error', swallowError)
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
@@ -61,90 +51,21 @@ gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
 });
 
+gulp.task('zip', ['css'], function () {
+    var targetDir = 'dist/';
+    var themeName = require('./package.json').name;
+    var filename = themeName + '.zip';
+
+    return gulp.src([
+        '**',
+        '!node_modules', '!node_modules/**',
+        '!dist', '!dist/**',
+        '!assets/css', '!assets/css/**'
+    ])
+        .pipe(zip(filename))
+        .pipe(gulp.dest(targetDir));
+});
+
 gulp.task('default', ['build'], function () {
     gulp.start('watch');
-});
-
-/*
- * Deployment tasks
- */
-gulp.task('zip', ['css'], function () {
-    // Include all files, except node_modules which are large and make this slow
-    return gulp.src(['**', '!node_modules/**'])
-    // Now also exclude everything mentioned in gitignore
-        .pipe(gitignore())
-        // Zip up what is left & save it
-        .pipe(zip(themeZipName))
-        .pipe(gulp.dest(themeZipPath));
-});
-
-function doUpload(url, bodyData) {
-    return got
-        .post(url + '/ghost/api/v0.1/authentication/token', {form: true, body: bodyData})
-        .then(function (res) {
-            var form = new FormData();
-
-            form.append('theme', fs.createReadStream(path.join(themeZipPath, themeZipName)));
-
-            return got
-                .post(url + '/ghost/api/v0.1/themes/upload', {
-                    headers: {
-                        Authorization: 'Bearer ' + JSON.parse(res.body).access_token
-                    },
-                    body: form
-                })
-                .then(function handleSuccess(res) {
-                    var theme = JSON.parse(res.body).themes[0];
-                    gutil.log(gutil.colors.green('Successfully uploaded: ' + theme.name));
-                    if (!theme.active) {
-                        gutil.log(gutil.colors.yellow('Warning: ' + theme.name + ' is not the active theme, please activate it to see changes.'));
-                    }
-                })
-                .catch(function handleError(err) {
-                    if (err.statusCode === 422) {
-                        var response = JSON.parse(err.response.body),
-                            error = response.errors[0];
-
-                        gutil.log(gutil.colors.red(error.errorType + ': ' + error.message));
-
-                        if (error.errorDetails && Array.isArray(error.errorDetails)) {
-                            gutil.log(gutil.colors.red('ErrorDetails: '));
-                            error.errorDetails.forEach(function (details) {
-                                gutil.log(details);
-                            });
-                        }
-                    } else {
-                        gutil.log(gutil.colors.red('Upload Error: ' + err));
-                    }
-
-                    gutil.beep();
-                });
-        }
-        )
-        .catch(function (err) {
-            gutil.log(gutil.colors.red('Auth Error: ' + err));
-            gutil.beep();
-        });
-}
-
-gulp.task('upload', function () {
-    try {
-        var deployConfig = require('./gulp-config.json');
-        var url = deployConfig.url;
-        var bodyData = {
-            grant_type: 'password',
-            client_id: deployConfig.client_id,
-            client_secret: deployConfig.client_secret,
-            username: deployConfig.username,
-            password: deployConfig.password
-        };
-    } catch (err) {
-        gutil.log(gutil.colors.red('Please copy gulp-config.json.example to gulp-config.json & fill out all of your details'));
-    }
-
-    return doUpload(url, bodyData);
-});
-
-gulp.task('deploy', ['zip'], function () {
-    gulp.start(['upload']);
 });
